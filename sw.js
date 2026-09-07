d9daa87dcaa8cdfdf2f49ee32eeab7a415eddd77
zek061061-cell <zek061061@gmail.com>
2026-09-07 21:55:40 +0300

    Refresh cache for mobile lazy loading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ters-kose-v2-shell-6";
+const CACHE="ters-kose-v2-shell-7";
 const CORE=["./","./manifest.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
