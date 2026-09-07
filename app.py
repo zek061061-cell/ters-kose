2762ce0d3b3d813bf740fc3e6c375610e8fc31e6
zek061061-cell <zek061061@gmail.com>
2026-09-07 17:37:41 +0300

    Align backend with deep-analysis model v4.0

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from urllib.parse import urlparse
 app = Flask(__name__)
 
 ALLOWED_HOSTS = {"www.football-data.co.uk", "football-data.co.uk"}
-APP_VERSION = "3.5"
+APP_VERSION = "4.0"
 SOURCE_CACHE = {}
 
 ESPN_LEAGUES = {
